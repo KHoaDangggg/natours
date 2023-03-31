9c16be8665e03f9165c2b1f6aaef4efe2535ae4a
Dang Khoa <khoa.dp210486@sis.hust.edu.vn>
2023-03-31 23:14:15 +0700

    Fix update

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -20,16 +20,20 @@ const upload = multer({
 });
 const uploadUserPhoto = upload.single('photo');
 const resizeUserPhoto = catchAsync(async (req, res, next) => {
-    if (!req.file) {
-        return next();
+    try {
+        if (!req.file) {
+            return next();
+        }
+        req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+        await sharp(req.file.buffer)
+            .resize(500, 500)
+            .toFormat('jpeg')
+            .jpeg({ quality: 90 })
+            .toFile(`public/img/users/${req.file.filename}`);
+        next();
+    } catch (error) {
+        console.log('error in resize');
     }
-    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-    await sharp(req.file.buffer)
-        .resize(500, 500)
-        .toFormat('jpeg')
-        .jpeg({ quality: 90 })
-        .toFile(`public/img/users/${req.file.filename}`);
-    next();
 });
 
 const getMe = (req, res, next) => {
