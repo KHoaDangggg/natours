478c7e7981a9e664a86f00adadfd0fe8b32b673d
Dang Khoa <khoa.dp210486@sis.hust.edu.vn>
2023-04-02 00:52:07 +0700

    Add auto complete search

--- public/js/login.mjs
+++ public/js/login.mjs
@@ -6,7 +6,7 @@ const showAlert = (type, msg) => {
     hideAlert();
     const markup = `<div class="alert alert--${type}">${msg}</div>`;
     document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
-    window.setTimeout(hideAlert, 5000);
+    window.setTimeout(hideAlert, 1500);
 };
 const login = async (email, password) => {
     try {
