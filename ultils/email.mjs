5e933a717474245b02d1464a70327287430fe31d
Dang Khoa <khoa.dp210486@sis.hust.edu.vn>
2023-04-09 22:23:49 +0700

    Fix email

--- ultils/email.mjs
+++ ultils/email.mjs
@@ -10,7 +10,7 @@ class Email {
         this.to = user.email;
         this.firstName = user.name.split(' ')[0];
         this.url = url;
-        this.from = 'DangPhucKhoa';
+        this.from = `DangPhucKhoa <${process.env.EMAIL_FROM}>`;
     }
 
     newTransport() {
